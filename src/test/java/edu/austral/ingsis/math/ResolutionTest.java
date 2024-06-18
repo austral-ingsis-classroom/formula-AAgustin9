daead69f8b3876b35b8fe418dcab5708adfa3e96
AAgustin9 <128090168+AAgustin9@users.noreply.github.com>
2024-06-18 12:46:35 -0300

    solve with vars

--- src/test/java/edu/austral/ingsis/math/ResolutionTest.java
+++ src/test/java/edu/austral/ingsis/math/ResolutionTest.java
@@ -14,7 +14,7 @@ public class ResolutionTest {
   /** Case 1 + 6 */
   @Test
   public void shouldResolveSimpleFunction1() {
-    final Double result = 7.0;
+    final Double result = 7d;
     Addition addition = new Addition(new Number(1), new Number(6));
     Double actual = addition.accept(visitor);
 
@@ -24,7 +24,7 @@ public class ResolutionTest {
   /** Case 12 / 2 */
   @Test
   public void shouldResolveSimpleFunction2() {
-    final Double result = 6.0;
+    final Double result = 6d;
     Division division = new Division(new Number(12), new Number(2));
     Double actual = division.accept(visitor);
 
@@ -54,7 +54,7 @@ public class ResolutionTest {
   /** Case 36 ^ (1/2) */
   @Test
   public void shouldResolveSimpleFunction5() {
-    final Double result = 6.0;
+    final Double result = 6d;
     Power power = new Power(new Number(36), new Division(new Number(1), new Number(2)));
     Double actual = power.accept(visitor);
 
@@ -64,7 +64,7 @@ public class ResolutionTest {
   /** Case |136| */
   @Test
   public void shouldResolveSimpleFunction6() {
-    final Double result = 136.0;
+    final Double result = 136d;
     AbsoluteValue abs = new AbsoluteValue(new Number(136));
     Double actual = abs.accept(visitor);
 
@@ -74,7 +74,7 @@ public class ResolutionTest {
   /** Case |-136| */
   @Test
   public void shouldResolveSimpleFunction7() {
-    final Double result = 136.0;
+    final Double result = 136.d;
     AbsoluteValue abs = new AbsoluteValue(new Number(-136));
     Double actual = abs.accept(visitor);
 
@@ -84,7 +84,7 @@ public class ResolutionTest {
   /** Case (5 - 5) * 8 */
   @Test
   public void shouldResolveSimpleFunction8() {
-    final Double result = 0.0;
+    final Double result = 0d;
     Multiplication multiplication = new Multiplication(new Subtraction(new Number(5), new Number(5)), new Number(8));
     Double actual = multiplication.accept(visitor);
 
--- src/test/java/edu/austral/ingsis/math/ResolutionWithVariablesTest.java
+++ src/test/java/edu/austral/ingsis/math/ResolutionWithVariablesTest.java
@@ -1,73 +1,106 @@
 package edu.austral.ingsis.math;
 
+import edu.austral.ingsis.math.operations.*;
+import edu.austral.ingsis.math.visitors.SolveVisitor;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ResolutionWithVariablesTest {
 
+  private final Map<String, Number> varMap = Map.of(
+          "x", new Number(3.0),
+          "div", new Number(4.0),
+          "y", new Number(4.0),
+          "a", new Number(9.0),
+          "b", new Number(3.0),
+          "z", new Number(36.0),
+          "value", new Number(8.0),
+          "i", new Number(2.0)
+  );
+
+  private final SolveVisitor visitor = new SolveVisitor(varMap);
+
   /** Case 1 + x where x = 3 */
   @Test
   public void shouldResolveFunction1() {
     final Double result = 4d;
+    Addition addition = new Addition(new Number(1), new Variable("x"));
+    Double actual = visitor.visit(addition);
 
-    assertThat(result, equalTo(4d));
+    assertThat(actual, equalTo(result));
   }
 
   /** Case 12 / div where div = 4 */
   @Test
   public void shouldResolveFunction2() {
     final Double result = 3d;
+    Division division = new Division(new Number(12), new Variable("div"));
+    Double actual = visitor.visit(division);
 
-    assertThat(result, equalTo(3d));
+    assertThat(actual, equalTo(result));
   }
 
   /** Case (9 / x) * y where x = 3 and y = 4 */
   @Test
   public void shouldResolveFunction3() {
     final Double result = 12d;
+    Multiplication multiplication = new Multiplication(new Division(new Number(9), new Variable("x")), new Variable("y"));
+    Double actual = visitor.visit(multiplication);
 
-    assertThat(result, equalTo(12d));
+    assertThat(actual, equalTo(result));
   }
 
   /** Case (27 / a) ^ b where a = 9 and b = 3 */
   @Test
   public void shouldResolveFunction4() {
     final Double result = 27d;
+    Power power = new Power(new Division(new Number(27), new Variable("a")), new Variable("b"));
+    Double actual = visitor.visit(power);
 
-    assertThat(result, equalTo(27d));
+    assertThat(actual, equalTo(result));
   }
 
   /** Case z ^ (1/2) where z = 36 */
   @Test
   public void shouldResolveFunction5() {
     final Double result = 6d;
+    Power power = new Power(new Variable("z"), new Division(new Number(1), new Number(2)));
+    Double actual = visitor.visit(power);
 
-    assertThat(result, equalTo(6d));
+    assertThat(actual, equalTo(result));
   }
 
   /** Case |value| - 8 where value = 8 */
   @Test
   public void shouldResolveFunction6() {
     final Double result = 0d;
+    Subtraction sub = new Subtraction(new AbsoluteValue(new Variable("value")), new Number(8));
+    Double actual = visitor.visit(sub);
 
-    assertThat(result, equalTo(0d));
+    assertThat(actual, equalTo(result));
   }
 
   /** Case |value| - 8 where value = 8 */
   @Test
   public void shouldResolveFunction7() {
     final Double result = 0d;
+    Subtraction sub = new Subtraction(new AbsoluteValue(new Variable("value")), new Number(8));
+    Double actual = visitor.visit(sub);
 
-    assertThat(result, equalTo(0d));
+    assertThat(actual, equalTo(result));
   }
 
   /** Case (5 - i) * 8 where i = 2 */
   @Test
   public void shouldResolveFunction8() {
     final Double result = 24d;
+    Multiplication multiplication = new Multiplication(new Subtraction(new Number(5), new Variable("i")), new Number(8));
+    Double actual = visitor.visit(multiplication);
 
-    assertThat(result, equalTo(24d));
+    assertThat(actual, equalTo(result));
   }
 }
