66e2b71ec486d45c54e7876a198236e44de7f5c8
AAgustin9 <128090168+AAgustin9@users.noreply.github.com>
2024-06-18 11:11:38 -0300

    implemented some visitors?

---
+++ src/main/java/edu/austral/ingsis/math/visitors/PrintVisitor.java
@@ -0,0 +1,53 @@
+package edu.austral.ingsis.math.visitors;
+
+import edu.austral.ingsis.math.Number;
+import edu.austral.ingsis.math.Variable;
+import edu.austral.ingsis.math.operations.*;
+
+public class PrintVisitor implements Visitor<String> {
+
+    @Override
+    public String visit(Addition addition) {
+        return "(" + addition.getLeft().accept(this) + " + " + addition.getRight().accept(this) + ")";
+    }
+
+    @Override
+    public String visit(Subtraction subtraction) {
+        return "(" + subtraction.getLeft().accept(this) + " - " + subtraction.getRight().accept(this) + ")";
+    }
+
+    @Override
+    public String visit(Multiplication multiplication) {
+        return "(" + multiplication.getLeft().accept(this) + " * " + multiplication.getRight().accept(this) + ")";
+    }
+
+    @Override
+    public String visit(Division division) {
+        return "(" + division.getDividend().accept(this) + " / " + division.getDivisor().accept(this) + ")";
+    }
+
+    @Override
+    public String visit(Power power) {
+        return "(" + power.getBase().accept(this) + " ^ " + power.getExponent().accept(this) + ")";
+    }
+
+    @Override
+    public String visit(SquareRoot squareRoot) {
+        return "sqrt(" + squareRoot.getFunction().accept(this) + ")";
+    }
+
+    @Override
+    public String visit(AbsoluteValue absoluteValue) {
+        return "|" + absoluteValue.getFunction().accept(this) + "|";
+    }
+
+    @Override
+    public String visit(Number number) {
+        return String.valueOf(number.getValue());
+    }
+
+    @Override
+    public String visit(Variable variable) {
+        return variable.getName();
+    }
+}
